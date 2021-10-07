bf1fed725cf3595de67112665f6bb5abbc15dd0b
Marina <montag.marinka@gmail.com>
2021-10-07 18:57:35 +0300

    created Ball

--- airhockey/main.py
+++ airhockey/main.py
@@ -1,24 +1,31 @@
 import pygame  # imports the package with all the available pygame modules
 import pymunk
-
-pygame.init()  # initializes each of pygame modules
-
-size = (width, height) = (1000, 600)
-
-display = pygame.display.set_mode(size)
-
-clock = pygame.time.Clock()
-fps = 50  # frames per second
+from classes import Ball
 
 
-def game():
+def pong_game(size=(1000, 600), fps=50, ball_radius=8, velocity=(400, -300)):
+    pygame.init()  # initializes each of pygame modules
+    display = pygame.display.set_mode(size)
+    space = pymunk.Space()  # contains all the information we need in our physics simulation
+    clock = pygame.time.Clock()
+    left = size[0] / 20
+    right = size[0] - left
+    top = size[1] / 24
+    bottom = size[1] - top
+    middleX = size[0] / 2
+    middleY = size[1] / 2
+    ball = Ball(display, space, x=middleX, y=middleY, radius=ball_radius, velocity=velocity)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        display.fill((0, 0, 0, 0))
+        ball.draw()
         pygame.display.update()
         clock.tick(fps)
+        space.step(1 / fps)
+    pygame.quit()
 
 
-game()
-pygame.quit()
+if __name__ == '__main__':
+    pong_game()
