6303504c9359573aff8c70be3ed42be5cb927058
Marina <montag.marinka@gmail.com>
2021-10-08 10:18:52 +0300

    forth version (completed scores and winner prints on display)

--- airhockey/classes.py
+++ airhockey/classes.py
@@ -1,9 +1,12 @@
+import random
+
 import pygame
 import pymunk
 
 
 class Ball:
-    def __init__(self, display, space, player_1, player_2, x=0, y=0, radius=10, velocity=(0, 0), density=1, elasticity=1,
+    def __init__(self, display, space, left, right, x=0, y=0, radius=10, velocity=[0, 0], density=1,
+                 elasticity=1,
                  collision_type=None, color=(255, 255, 255)):
         self.display = display
         self.space = space
@@ -21,26 +24,47 @@ class Ball:
         if collision_type is not None:
             self.shape.collision_type = collision_type
         self.space.add(self.body, self.shape)
-        self.player_1 = player_1
-        self.player_2 = player_2
+        self.left = left
+        self.right = right
 
     def draw(self):
         x, y = self.body.position
         pygame.draw.circle(self.display, self.color, (int(x), int(y)), self.radius)
 
-    def to_start_position(self, space, arbiter, data):
+    def left_won(self, space, arbiter, data):
+        self.to_start_position()
+        self.left.score += 1
+        print('Left:', self.left.score)
+        if self.left.score == self.left.max_score:
+            self.left.winner = True
+            self.right.winner = False
+            print('Winner Left!!!')
+        return True
+
+    def right_won(self, space, arbiter, data):
+        self.to_start_position()
+        self.right.score += 1
+        print('Right:', self.right.score)
+        if self.right.score == self.right.max_score:
+            self.right.winner = True
+            self.left.winner = False
+            print('Winner Right!!!')
+        return True
+
+    def to_start_position(self):
         self.body.position = (self.x, self.y)
         self.body.velocity = (0, 0)
-        self.player_1.to_start_position()
-        self.player_2.to_start_position()
-        return False
+        self.left.to_start_position()
+        self.right.to_start_position()
 
     def move(self):
+        self.velocity[0] = self.velocity[0] * random.choice([-1, 1])
+        self.velocity[1] = self.velocity[1] * random.choice([-1, 1])
         self.body.velocity = self.velocity
 
 
 class Wall:
-    def __init__(self, display, space, start, end, collision_type=None, elasticity=1, thickness=10,
+    def __init__(self, display, space, start, end, collision_type=None, elasticity=1, thickness=8,
                  color=(255, 255, 255), group=None):
         self.display = display
         self.space = space
@@ -62,21 +86,25 @@ class Wall:
 
 
 class Player:
-    def __init__(self, display, space, x, offset=30, thickness=10, color=(255, 255, 255), elasticity=1,
-                 collision_type=None, group=None):
+    def __init__(self, display, space, x, center_offset=30, thickness=10, color=(255, 255, 255), elasticity=1,
+                 collision_type=None, group=None, max_score=3, winner=None, velocity=800):
         self.display = display
         self.space = space
         self.thickness = thickness
         self.color = color
         self.x = x
-        self.offset = offset
+        self.center_offset = center_offset
         middleY = self.display.get_height() / 2
         self.body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
         self.body.position = (self.x, middleY)
-        self.start = [0, -offset]
-        self.end = [0, offset]
+        self.start = [0, -center_offset]
+        self.end = [0, center_offset]
         self.shape = pymunk.Segment(self.body, self.start, self.end, self.thickness)
         self.shape.elasticity = elasticity
+        self.score = 0
+        self.max_score = max_score
+        self.winner = winner
+        self.velocity = velocity
         if collision_type is not None:
             self.shape.collision_type = collision_type
         if group is not None:
@@ -90,12 +118,21 @@ class Player:
 
     def move(self, up=True):
         if up:
-            self.body.velocity = (0, -600)
+            self.body.velocity = (0, -self.velocity)
         else:
-            self.body.velocity = (0, 600)
+            self.body.velocity = (0, self.velocity)
 
     def stop(self):
         self.body.velocity = (0, 0)
 
     def to_start_position(self):
         self.body.position = (self.x, self.display.get_height() / 2)
+
+    def on_edge(self, top, bottom):
+        if self.body.local_to_world(self.shape.a)[1] <= top:
+            self.body.velocity = (0, 0)
+            self.body.position = (self.body.position[0], top + self.center_offset)
+        if self.body.local_to_world(self.shape.b)[1] >= bottom:
+            self.body.velocity = (0, 0)
+            self.body.position = (self.body.position[0], bottom - self.center_offset)
+
--- airhockey/main.py
+++ airhockey/main.py
@@ -24,11 +24,10 @@ def play(keys, up, down, player, ball, top, bottom):
                 player.stop()
 
 
-def game_over(player_1, player_2):
-    if player_1.winner or player_2.winner:
-        pygame.quit()
-        return True
-    return False
+def print_text(display, text, x, y, font_size, bold=True, italic=False, color=(255, 255, 255)):
+    font = pygame.font.SysFont('Algerian', font_size, bold, italic)
+    surface = font.render(text, True, color)
+    display.blit(surface, (x, y))
 
 
 def pong_game(size=(1000, 600), fps=100, ball_radius=10, velocity=[700, 300], goal_left_collision_type=1,
@@ -50,6 +49,8 @@ def pong_game(size=(1000, 600), fps=100, ball_radius=10, velocity=[700, 300], go
     goal_bottom = goal_top + goal_height
     player_1_pos = left + offset
     player_2_pos = right - offset
+    text_score_size = 30
+    congrats_text_size = int(size[1]/5)
 
     player_1 = Player(display, space, player_1_pos, thickness=player_thickness, velocity=player_velocity)
     player_2 = Player(display, space, player_2_pos, thickness=player_thickness, velocity=player_velocity)
@@ -82,28 +83,33 @@ def pong_game(size=(1000, 600), fps=100, ball_radius=10, velocity=[700, 300], go
                 return
 
         display.fill(field_color)
-        ball.draw()
-        wall_left_top.draw()
-        wall_left_bottom.draw()
-        wall_right_top.draw()
-        wall_right_bottom.draw()
-        wall_top.draw()
-        wall_bottom.draw()
-        goal_left.draw()
-        goal_right.draw()
-        player_1.draw()
-        player_2.draw()
-        pygame.draw.line(display, center_line_color, [middleX, top], [middleX, bottom], center_line_thickness)
-        if game_over(player_1, player_2):
-            return
+        if not player_1.winner and not player_2.winner:
+            ball.draw()
+            wall_left_top.draw()
+            wall_left_bottom.draw()
+            wall_right_top.draw()
+            wall_right_bottom.draw()
+            wall_top.draw()
+            wall_bottom.draw()
+            goal_left.draw()
+            goal_right.draw()
+            player_1.draw()
+            player_2.draw()
+            pygame.draw.line(display, center_line_color, [middleX, top], [middleX, bottom], center_line_thickness)
 
-        keys = pygame.key.get_pressed()
+            print_text(display, f'Score: {player_1.score}', left, 5, text_score_size)
+            print_text(display, f'Score: {player_2.score}', right - 100, 5, text_score_size)
 
-        play(keys, pygame.K_UP, pygame.K_DOWN, player_2, ball, top, bottom)
-        play(keys, pygame.K_w, pygame.K_s, player_1, ball, top, bottom)
+            keys = pygame.key.get_pressed()
 
-        restart_round_if_ended(keys, ball)
+            play(keys, pygame.K_UP, pygame.K_DOWN, player_2, ball, top, bottom)
+            play(keys, pygame.K_w, pygame.K_s, player_1, ball, top, bottom)
 
+            restart_round_if_ended(keys, ball)
+        elif player_1.winner:
+            print_text(display, 'Winner is Player1', left, int(size[1]/3), congrats_text_size)
+        else:
+            print_text(display, 'Winner is Player2', left, int(size[1]/3), congrats_text_size)
         pygame.display.update()
         clock.tick(fps)
         space.step(1 / fps)
